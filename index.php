c62a2eee382e85ed528f5891db7dae660c810d54
Adrià Felip <adria.felip@ibadia.cat>
2015-12-10 16:52:11 +0100

    PR10 Client PHP contra GitHub (PROJECTE)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //curl -X GET -u :5d82061c073c52d46820b60d79cb0816a5c16e54 https://api.github.com/user/repos --> Consulta para comprobar nuestro TOKEN
 include_once './class/curl.class.php';
 
-$curl = new Curl(); // Creamos un objeto Curl
+$curl = new Curl(); // Creamos un objeto curl
 $token = "15ae0c9babd7a901da929b1950d32b84a0d81baf"; // Creamos nuestro TOKEN que hemos creado en GitHub
 if (!isset($_REQUEST['repository'])) { // si no nos a entrado una peticion con indice repository 
     repositorios($curl, $token); // Vemos todos los repositorios 
